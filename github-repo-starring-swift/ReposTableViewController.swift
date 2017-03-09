fa235d839129b5b884c233e7a0e1cf28c23800b4
edmund-h <447ght@gmail.com>
2017-03-09 13:27:58 -0500

    works. no tests.

--- github-repo-starring-swift/ReposTableViewController.swift
+++ github-repo-starring-swift/ReposTableViewController.swift
@@ -23,6 +23,11 @@ class ReposTableViewController: UITableViewController {
                 self.tableView.reloadData()
             })
         }
+        GithubAPIClient.starFunctions(task: GitHubFunctions.checkStarred, repoUrl:"immersedreality/swift-HelloToTheUniverse-lab-swift-intro-000" , completion: {resultCode in
+            
+            print("initial starCheck complete with code \(resultCode)")
+            print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+        })
     }
 
     // MARK: - Table view data source
@@ -37,8 +42,42 @@ class ReposTableViewController: UITableViewController {
 
         let repository:GithubRepository = self.store.repositories[(indexPath as NSIndexPath).row]
         cell.textLabel?.text = repository.fullName
-
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let repoDirectory = store.repositories[indexPath.row].fullName
+        GithubAPIClient.starFunctions(task: GitHubFunctions.checkStarred, repoUrl: repoDirectory, completion: {
+            responseCode in
+            switch responseCode{
+            case 404:
+                GithubAPIClient.starFunctions(task: GitHubFunctions.addStar, repoUrl: repoDirectory, completion: { result in
+                    print ("attempted starring with result:\(result)")
+                    self.makeAlert(say: "You have starred \(repoDirectory)!")
+                    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+                })
+            case 204:
+                GithubAPIClient.starFunctions(task: GitHubFunctions.unStar, repoUrl: repoDirectory, completion: { result in
+                    print ("attempted unStarring with result:\(result)")
+                    self.makeAlert(say: "You have un-starred \(repoDirectory)!")
+                    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+                })
+            default:
+                print ("something has gone terribly wrong")
+            }
+        })
+    }
+}
 
+extension ReposTableViewController {
+    func makeAlert (say: String){
+        let starredAlert = UIAlertController(title: "", message: say, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { (okAction) in
+            print ("user acknowledged message")
+            return
+        }
+        starredAlert.addAction(okAction)
+        self.present(starredAlert, animated: true, completion: {print("user notified: \(starredAlert.message)")})
+        
+    }
 }
